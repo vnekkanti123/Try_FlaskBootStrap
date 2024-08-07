da75f76324de639dbc830dcd0ea0557cc9b53d66
Heisenberg1719 <heisenberg@khazanapay.in>
2024-08-07 21:49:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 from flask import Flask, send_from_directory, render_template
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
 
-@app.route('/')
-def home():
-    return render_template('login.html')
+class MyApp:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app.config['SECRET_KEY'] = "your_default_secret_key"
+        self._setup_routes()
 
-@app.route('/admin/dashBoard' , methods=['GET'])
-def dashBoard():
-    return render_template('dashBoard.html')
+    def _setup_routes(self):
+        @self.app.route('/')
+        def home():
+            return render_template('login.html')
 
-@app.route('/admin/userWalletAndKycUpdate' , methods=['GET'])
-def userWalletKycUpdate():
-    return render_template('manageUsers.html')
+        @self.app.route('/admin/dashBoard', methods=['GET'])
+        def dashBoard():
+            return render_template('dashBoard.html')
 
-@app.route('/assets/<path:filename>')
-def serve_assets(filename):
-    return send_from_directory('assets', filename)
+        @self.app.route('/admin/userWalletAndKycUpdate', methods=['GET'])
+        def userWalletKycUpdate():
+            return render_template('manageUser.html')
 
+        @self.app.route('/assets/<path:filename>')
+        def serve_assets(filename):
+            return send_from_directory('assets', filename)
 
-@app.route('/JQuerySetUp/<path:filename>')
-def serve_JQuerySetUp(filename):
-    return send_from_directory('JQuerySetUp', filename)
-
-
-@app.route('/templates/<path:filename>')
-def serve_JQuery(filename):
-    return send_from_directory('templates', filename)
+    def run(self):
+        self.app.run(host='0.0.0.0', port=8080, debug=False)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app_instance = MyApp()
+    app_instance.run()
